605f4985e8cfb1b9ef8434f7df9ee8ca6739a867
Ronak Shah <shahronak1996@gmail.com>
2017-11-14 19:18:32 -0600

    refactored UI

--- src/navbar/MainNavbar.js
+++ src/navbar/MainNavbar.js
@@ -16,17 +16,10 @@ class MainNavbar extends Component {
                     <Nav>
                         <NavItem eventKey={1} href="/">Link</NavItem>
                         <NavItem eventKey={2} href="/">Link</NavItem>
-                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                        <MenuItem eventKey={3.1}>Action</MenuItem>
-                        <MenuItem eventKey={3.2}>Another action</MenuItem>
-                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                        <MenuItem divider />
-                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                        </NavDropdown>
                     </Nav>
                     <Nav pullRight>
-                        <NavItem eventKey={1} href="/">Link Right</NavItem>
-                        <NavItem eventKey={2} href="/">Link Right</NavItem>
+                        <NavItem eventKey={1} href="/">Login</NavItem>
+                        <NavItem eventKey={2} href="/">Register</NavItem>
                     </Nav>
                     </Navbar.Collapse>
                 </Navbar>
